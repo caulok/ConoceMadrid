157fc641cd9ac551646924007cb11926efcc718d
caulok <ramicaulo@gmail.com>
2022-06-07 10:45:32 -0300

    sumando el archivo js

---
+++ js/main.js
@@ -0,0 +1,16 @@
+// // Saludo
+// const SALUDO = "Hola ";
+// let nombre = prompt('Ingresa tu nombre');
+// const RESTO = ", bienvenida al mejor sitio del mundo. PD: Te amo, aguante bokita (L)";
+
+// resultadoSaludos = SALUDO + nombre + RESTO;
+
+// alert(resultadoSaludos);
+
+// Suma del usuario
+// let valorUno = prompt('Ingresa un número');
+// let valorDos = prompt('Ingresa un segundo número');
+
+// resultadoSuma = parseFloat(valorUno) + parseFloat(valor2);
+
+// alert('El resultado es = ' + resultadoSuma);
